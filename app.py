fa0aed411615caef3772240dcc1a272156880f53
runde3wisc <47787784+runde3wisc@users.noreply.github.com>
2022-06-11 12:08:10 -0500

    feat: added warehouse front end and back end

--- app.py
+++ app.py
@@ -15,56 +15,107 @@ db = SQLAlchemy(app)
 
 class InventoryItem(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100))
-    email = db.Column(db.String(100))
-    phone = db.Column(db.String(20))
+    name = db.Column(db.String(100), unique=True, nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+    
+    warehouse_id = db.Column(db.Integer, db.ForeignKey('warehouse.id'), nullable=True)
+    warehouse = db.relationship('Warehouse', back_populates='inventory_items')
 
-    def __init__(self, name, email, phone):
+    def __init__(self, name, quantity):
         self.name = name
-        self.email = email
-        self.phone = phone
+        self.quantity = quantity
 
+
+class Warehouse(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True, nullable=False)
+
+    inventory_items = db.relationship('InventoryItem', back_populates='warehouse')
+
+    def __init__(self, name):
+        self.name = name
 
 
 @app.route('/')
 def Index():
-    all_data = InventoryItem.query.all()
-    return render_template("index.html", data=all_data)
+    inventory_data = InventoryItem.query.all()
+    warehouse_data = Warehouse.query.all()
+    return render_template("index.html", inventory_data=inventory_data, warehouse_data=warehouse_data)
 
-@app.route('/insert', methods=['POST'])
-def insert():
+
+@app.route('/insert_item', methods=['POST'])
+def insert_item():
     if request.method == 'POST':
         name = request.form['name']
-        email = request.form['email']
-        phone = request.form['phone']
+        quantity = request.form['quantity']
 
-        data = InventoryItem(name, email, phone)
+        data = InventoryItem(name, quantity)
         db.session.add(data)
         db.session.commit()
 
-        flash("Inserted Successfully")
-
+        flash("Inserted Inventory Item Successfully")
         return redirect(url_for('Index'))  # redirect user back to index
 
-@app.route('/update', methods=['GET', 'POST'])
-def update():
+
+@app.route('/update_item', methods=['GET', 'POST'])
+def update_item():
     if request.method == 'POST':
         data = InventoryItem.query.get(request.form.get('id'))  # the hidden ID from the form
         data.name = request.form['name']
-        data.email = request.form['email']
-        data.phone = request.form['phone']
+        data.quantity = request.form['quantity']
 
         db.session.commit()
-        flash("Updated Successfully")
+
+        flash("Updated Inventory Item Successfully")
         return redirect(url_for('Index'))
 
-@app.route('/delete/<id>/', methods=['GET', 'POST'])
-def delete(id):
+
+@app.route('/delete_item/<id>/', methods=['GET', 'POST'])
+def delete_item(id):
     data = InventoryItem.query.get(id)
+
     db.session.delete(data)
     db.session.commit()
-    flash("Deleted Successfully")
+
+    flash("Deleted Inventory Item Successfully")
     return redirect(url_for('Index'))
 
+
+@app.route('/insert_warehouse', methods=['POST'])
+def insert_warehouse():
+    if request.method == 'POST':
+        name = request.form['name']
+
+        data = Warehouse(name)
+        db.session.add(data)
+        db.session.commit()
+
+        flash("Inserted Warehouse Successfully")
+
+        return redirect(url_for('Index'))  # redirect user back to index
+
+
+@app.route('/update_warehouse', methods=['GET', 'POST'])
+def update_warehouse():
+    if request.method == 'POST':
+        data = Warehouse.query.get(request.form.get('id'))  # the hidden ID from the form
+        data.name = request.form['name']
+
+        db.session.commit()
+        flash("Updated Warehouse Successfully")
+        return redirect(url_for('Index'))
+
+
+@app.route('/delete_warehouse/<id>/', methods=['GET', 'POST'])
+def delete_warehouse(id):
+    data = Warehouse.query.get(id)
+    db.session.delete(data)
+    db.session.commit()
+    flash("Deleted Warehouse Successfully")
+    return redirect(url_for('Index'))
+
+
 if __name__ == "__main__":
+    db.drop_all()  # clear db tables for development purposes
+    db.create_all()  # create database tables
     app.run(debug=True)
